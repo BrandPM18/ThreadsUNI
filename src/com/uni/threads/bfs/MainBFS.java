fd9c06a610ff30d5885b38f44877ab4598124ce7
Brando <bpalaciosm@uni.pe>
2021-04-22 00:14:52 -0500

    BFS secuencial part 1

--- src/com/uni/threads/bfs/MainBFS.java
+++ src/com/uni/threads/bfs/MainBFS.java
@@ -3,9 +3,18 @@ package com.uni.threads.bfs;
 /*
  * Carpeta donde se deben comparar ambos codigos importanto ParallelBFS y SecuencialBFS
 */
+import java.lang.*;
 
 public class MainBFS {
     public static void main(String[] args) {
-
+        int n = 1000;
+        SecuencialBFS FirstBFS = new SecuencialBFS(n);
+        FirstBFS.generateEdge();
+        long start = System.currentTimeMillis();
+        int searchI= (int) Math.random()*n;
+        System.out.println("Start : "+searchI);
+        FirstBFS.searchBFS(searchI);
+        long end = System.currentTimeMillis();
+        System.out.println("Generate :"+(end-start));
     }
 }
--- src/com/uni/threads/bfs/SecuencialBFS.java
+++ src/com/uni/threads/bfs/SecuencialBFS.java
@@ -1,4 +1,61 @@
 package com.uni.threads.bfs;
 
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SecuencialBFS {
+    private  int vertx; // Numero de vertices
+    private LinkedList<Integer>[]  graph; // Matriz de abyacencia
+    private LinkedList<Integer> solution = new LinkedList<>(); // Almacen de solucion
+
+    public SecuencialBFS(int size) {
+        this.vertx = size;
+        this.graph = new LinkedList[vertx];
+        for(int i= 0;i<vertx;i++)
+            graph[i] = new LinkedList<>();
+    }
+    public void addEdges(int[] conex){
+        graph[conex[0]].add(conex[1]);
+    }
+
+    public boolean notExistEdges(int vxt1,int vxt2){
+        return  (graph[vxt1].indexOf(vxt2)==-1 )|| (graph[vxt2].indexOf(vxt1)==-1 ) ;
+    }
+    public void generateEdge() {
+        int rand1=0,rand2=0;
+        int edgeMax = vertx*(vertx-1)/2;
+        for (int i = 0; i < edgeMax; i++) {
+            rand1 = (int) (Math.random() * vertx);
+            rand2 = (int) (Math.random() * vertx);
+            System.out.println(rand1 + "..." + rand2);
+            if ((rand1 == rand2) || !(notExistEdges(rand1, rand2))) {
+                i--;
+                System.out.println("Pass");
+                continue;
+            }
+            addEdges(new int[]{rand1, rand2});
+        }
+    }
+
+
+    public void searchBFS(int num){
+        boolean pass[] = new boolean[vertx];
+        pass[num] = true;
+        solution.addLast(num);
+        while(!solution.isEmpty()){
+            int temp = solution.poll();
+            System.out.print(temp + "-");
+            for(int i=0;i<graph[temp].size();i++){
+               if(!pass[graph[temp].get(i)]){
+                   solution.addLast(graph[temp].get(i));
+                   pass[graph[temp].get(i)] = true;
+               }
+            }
+        }
+        System.out.println(" ");
+    }
+
+
 }
