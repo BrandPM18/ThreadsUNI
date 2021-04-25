69415f1ab1fecbd62e50385efbe895f8eedbabf8
cesar-yaranga <juliocesar3653@gmail.com>
2021-04-25 17:15:02 -0500

    Se agrego el ParallelMergSort y sus resultados

--- src/com/uni/threads/comparesort/parallel/ParallelMergeSort.java
+++ src/com/uni/threads/comparesort/parallel/ParallelMergeSort.java
@@ -1,10 +1,129 @@
-package com.uni.threads.comparesort.parallel;
 /*
- * Aqui usamos el metodo usando hilos "Threads" puedes ingresar una clase del merge secuencial si es necesario
- * No coloques una funcion main aqui, corre el codigo en ParallelSortVector
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-public class ParallelMergeSort extends Thread{
+package com.mycompany.tareagrupal;
 
+import java.util.Calendar;
+
+/**
+ *
+ * @author Cesar
+ */
+public class ParallelMergeSort {
+    
+    private double datos;
+    private long tiempo;
+    private int numOfThreads;
+    
+    public void setNumOfThreads(int numOfThreads)
+    {
+        this.numOfThreads = numOfThreads;
+    }
+    
+    public void setDatos(double datos)
+    {
+        this.datos = datos;
+    }
+    
+    public long getTiempo(){
+        return tiempo;
+    }
+    
+    public void merge(int A[], int izq, int m, int der)
+    {
+        int i, j, k;
+        int[] B = new int[A.length];
+        
+        for(i=izq; i<=der; i++){
+            B[i] = A[i];
+        }
+        
+        i=izq;
+        j=m+1;
+        k=izq;
+        
+        while(i<=m && j<=der){
+            if(B[i] <= B[j]){
+                A[k++] = B[i++];
+            }else{
+                A[k++] = B[j++];
+            }
+        }
+        
+        while(i<=m){
+            A[k++] = B[i++];
+        }
+    }
+    
+    public void mergesort(int A[], int izq, int der)
+    {
+        if(izq < der){
+            int m=(izq+der)/2;
+            mergesort(A, izq, m);
+            mergesort(A, m+1, der);
+            merge(A, izq, m, der);
+        }
+    }
+    
+    private Thread mergeSortParallel(int[] A, int low, int high, int numOfThreads)
+    {
+        return new Thread()
+        {
+            @Override
+            public void run()
+            {
+                ParallelMergeSort(A, low, high, numOfThreads/2);
+            }
+        };
+    }
+    
+    public void ParallelMergeSort(int[] A, int izq, int der, int numOfThreads)
+    {
+        if(numOfThreads <= 1)
+        {
+            mergesort(A, izq, der);
+            return;
+        }
+        
+        int m = (izq + der)/2;
+        
+        Thread leftSorter = mergeSortParallel(A, izq, m, numOfThreads);
+        Thread rightSorter = mergeSortParallel(A, m+1, der, numOfThreads);
+        
+        leftSorter.start();
+        rightSorter.start();
+        
+        try{
+            leftSorter.join();
+            rightSorter.join();
+        }catch(InterruptedException e){}
+        
+        merge(A, izq, m, der);
+        
+    }
+    
+    public void ordenar()
+    {
+        int ints[] = new int[(int)datos];
+        
+        for (int i=0; i<ints.length; i++)
+        {
+            ints[i]= (int)(Math.random()*datos);
+        }
+        
+        long start;
+        long end;
+        
+        int der = ints.length/2;
+        int izq = der-1;
+        
+        start = Calendar.getInstance().getTimeInMillis();
+        ParallelMergeSort(ints, izq, der, numOfThreads);
+        end = Calendar.getInstance().getTimeInMillis();
+        
+        this.tiempo = (end - start)/100;
+    }
+    
 }
-
-// https://github.com/dongpytran/Thread-Sort-Java-Parallel/blob/main/DoAnXuLySongSong/src/Merger.java
