d5ca4867408023831fdcaaedc7c6f1ed75a29777
cesar-yaranga <juliocesar3653@gmail.com>
2021-04-24 20:31:32 -0500

    Se realizo el ArraySort como el ParallelSort, agregando los resultados de ambos tambien

--- src/com/uni/threads/comparesort/ParallelSortVector.java
+++ src/com/uni/threads/comparesort/ParallelSortVector.java
@@ -4,6 +4,18 @@ package com.uni.threads.comparesort;
  *  Aqui pondremos un metodo estatico que invoque a  la funcion Arrays.parallelSort  retornaremos el tiempo que tarda
         //    Arrays.parallelSort()
 
+	//	Cree 2 funciones utilizando el ordenamiento de vectores Arrays.sort y Arrays.parallelSort
+	//
+	//	ParallelSort ps = New ParallelSort();
+	//	ps.setDatos(dato);
+	//	ps.ordenar();
+	//	ps.getTiempo();	
+
+	//	ArraySort as = new ArraySort();
+	//	as.setDatos(dato);
+	//	as.ordenar();
+	//	as.getTiempo();
+
  * Importamos la clase parallelmergesort para usar el metodo con hilos comparamos todos los datos.
  * */
 
---
+++ src/com/uni/threads/comparesort/sequential/ArraySort.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tareagrupal;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Cesar
+ */
+public class ArraySort {
+    
+    private double datos;
+    private double tiempo;
+    
+    public void setDatos(double datos)
+    {
+        this.datos = datos;
+    }
+    
+    public double getTiempo(){
+        return tiempo;
+    }
+    
+    public void ordenar()
+    {
+        int ints[] = new int[(int)datos];
+        
+        for (int i=0; i<ints.length; i++)
+        {
+            ints[i]= (int)(Math.random()*datos);
+        }
+        
+        double start;
+        double end;
+        
+        start = System.currentTimeMillis();
+        Arrays.sort(ints);
+        end = System.currentTimeMillis();
+        
+        this.tiempo = (end - start)/100;
+        
+        System.out.println("*****************************************");
+        System.out.println("Tiempo: "+(end - start)/100);
+        System.out.println("*****************************************");
+        
+    }
+    
+}
