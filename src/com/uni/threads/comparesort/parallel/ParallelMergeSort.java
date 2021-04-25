2e25ba1e9b25f36fa10b5a9fb95e933905dfb121
cesar-yaranga <juliocesar3653@gmail.com>
2021-04-25 17:16:59 -0500

    Solo se modificó ParallelMergSort

--- src/com/uni/threads/comparesort/parallel/ParallelMergeSort.java
+++ src/com/uni/threads/comparesort/parallel/ParallelMergeSort.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.tareagrupal;
+package com.uni.threads.comparesort.parallel;
 
 import java.util.Calendar;
 
